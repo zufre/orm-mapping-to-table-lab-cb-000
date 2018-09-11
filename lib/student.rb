729a1643b2869a29dfc536db4d95fcc0f9818582
Cedric David <cedric.david@btopenworld.com>
2018-09-11 02:47:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,8 +28,9 @@ class Student
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
-      VALUES (?. ?)
+      VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 end
