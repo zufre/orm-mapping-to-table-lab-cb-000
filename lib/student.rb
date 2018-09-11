b14b55a59ee15a1a8fd2e689390c98023ca9845b
Cedric David <cedric.david@btopenworld.com>
2018-09-11 02:45:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,4 +25,11 @@ class Student
     SQL
     DB[:conn].execute(sql)
   end
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?. ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
 end
